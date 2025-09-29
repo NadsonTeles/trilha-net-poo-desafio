646fc0e2bda2c565e16478bdaf83c738159f351f
NadsonTeles <nd7losttower7@gmail.com>
2025-09-29 17:46:54 -0300

    desafio_avanade

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("=== Smartphone Nokia ===");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("=== Smartphone iPhone ===");
+Smartphone iphone = new Iphone(numero: "789012", modelo: "iPhone 12", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine();
+Console.WriteLine("Testes realizados com sucesso!");
